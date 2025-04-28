e61bdfee1461420947253a0b6c686570ed37f21c
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2025-04-29 05:19:01 +0530

    Update: Eslint config for JSX

--- 01-no-frills-react/eslint.config.mjs
+++ 01-no-frills-react/eslint.config.mjs
@@ -1,12 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+
+// Order of the configs is important. The last config will override the previous ones if you do any dumb thing.
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx"], // remember to add the file extensions you want to lint
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -14,6 +26,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
