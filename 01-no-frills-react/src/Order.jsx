fdd8662bee4e0c9d051e79b98a8ab4cd7ba11e9a
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2025-04-29 06:42:55 +0530

    Update: reponding to side effects outside the component using 'useEffect' s

--- 01-no-frills-react/src/Order.jsx
+++ 01-no-frills-react/src/Order.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pizza from "./Pizza";
+
+const intl = new Intl.NumberFormat("en-LK", {
+  style: "currency",
+  currency: "LKR",
+});
 
 export default function Order() {
   // const pizzaType = "pepperoni";
   // const pizzaSize = "M";
 
+  const [pizzaTypes, setPizzaTypes] = useState([]);
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M");
+  const [loading, setLoading] = useState(true);
+
+  let prize, selectedPizza;
+
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+  }
+
+  async function fetchPizzaTypes() {
+    const pizzaResponse = await fetch("/api/pizzas");
+    const pizzaJson = await pizzaResponse.json();
+    setPizzaTypes(pizzaJson);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []);
 
   const handlePizzaSizeChange = (event) => {
     setPizzaSize(event.target.value);
-  }
+  };
   return (
     <div className="order">
       <h2>Create Order</h2>
@@ -25,9 +49,13 @@ export default function Order() {
               name="pizza-type"
               value={pizzaType}
             >
-              <option value="pepperoni">The Pepperoni Pizza</option>
-              <option value="hawaiian">The Hawaiian Pizza</option>
-              <option value="big_meat">The Big Meat Pizza</option>
+              {pizzaTypes.map((pizza) => { 
+                return (
+                  <option key={pizza.id} value={pizza.id}>
+                    {pizza.name}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <div>
